a0103ea2d33b45983bd9fd935df4ab5459daf41f
Jørgen Juel <jorgen.juel@hotmail.com>
2019-02-08 11:23:05 +0100

    Forbedret enkeltjobb

--- resources/views/partials/content-single-jobb.blade.php
+++ resources/views/partials/content-single-jobb.blade.php
@@ -1,19 +1,22 @@
 <article @php post_class('tekstinnhold') @endphp>
+
     <header>
       @if(has_post_thumbnail())
         <div class="tekstinnhold__header__logo">
           {!! get_the_post_thumbnail( null, 'logo', '' ) !!}
         </div>
       @endif
-      <h1>{{ get_the_title() }}</h1>
-
-      
+      <h1>{{ get_the_title() }}</h1>      
     </header>
+
+    <aside class="tekstinnhold__sideboks">
+      <div class="tekstinnhold__sideboks__logo">
+        {!! $bedriftslogo !!}
+      </div>
+    </aside>
+    
+
     <div class="tekstinnhold__tekst">
       @php the_content() @endphp
     </div>
-
-    <aside class="tekstinnhold__sideboks">
-      {!! $bedriftslogo !!}
-    </aside>
 </article>
